f85444b0c3fe78a7349ecfd1a5d0c48f18eb41e2
Ajit_Kumar_Singh <ajit.sumitsingh@gmail.com>
2015-07-19 21:11:54 +0530

    Some refactoring

--- app/controllers/warehouses_controller.rb
+++ app/controllers/warehouses_controller.rb
@@ -30,7 +30,7 @@ class WarehousesController < ApplicationController
 
     respond_to do |format|
       if @warehouse.save
-        format.html { redirect_to @warehouse, notice: 'Warehouse was successfully created.' }
+        format.html { redirect_to @warewarehouse, notice: 'Warehouse was successfully created.' }
         format.json { render :show, status: :created, location: @warehouse }
       else
         format.html { render :new }
@@ -44,7 +44,7 @@ class WarehousesController < ApplicationController
   def update
     respond_to do |format|
       if @warehouse.update(warehouse_params)
-        format.html { redirect_to @warehouse, notice: 'Warehouse was successfully updated.' }
+        format.html { redirect_to (:back), notice: 'Warehouse was successfully updated.' }
         format.json { render :show, status: :ok, location: @warehouse }
       else
         format.html { render :edit }
