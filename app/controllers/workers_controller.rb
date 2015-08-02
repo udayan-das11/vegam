3464a016680bbbcbc6da5654797aea7197121783
Ajit_Kumar_Singh <ajit.sumitsingh@gmail.com>
2015-08-02 14:14:28 +0530

    Workers controller

--- app/controllers/workers_controller.rb
+++ app/controllers/workers_controller.rb
@@ -42,18 +42,20 @@ class WorkersController < ApplicationController
     @city =City.find(params[:worker][:city])
     puts("***************Debugging******************")
     @worker.city=@city.cityName
-
     subCityName=""
-    numServices=params.size-7
-
-    for i in 0..numServices
-      subAreaField="location"+i.to_s;
-      if (!WorkerLocalityMapping.exists?(worker_id:params[:worker_id],locality_id:params[subAreaField]))
-        workerLocalityMapping=WorkerLocalityMapping.new(worker_id:@worker.id,locality_id:params[subAreaField]);
-        subCityName=subCityName+ ";"+Locality.find(params[subAreaField]).subCity.to_s
-        workerLocalityMapping.save
+    if(@worker.save)
+      params.each do |key, value|
+        # target groups using regular expressions
+        if (key.to_s[/location.*/])
+          if (!WorkerLocalityMapping.exists?(worker_id:@worker.id,locality_id:key.to_s))
+            workerLocalityMapping=WorkerLocalityMapping.new(worker_id:@worker.id,locality_id:params[key.to_s]);
+            subCityName=subCityName+ "    "+Locality.find(params[key.to_s]).subCity.to_s
+            workerLocalityMapping.save
+          end
+        end
       end
     end
+
     @worker.subcity=subCityName
 
     respond_to do |format|
